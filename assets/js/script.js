aea27b3f68463aa2745db518f63d00e9e00d1f15
OmarD <omar.daghestani@colorado.edu>
2022-10-22 11:16:27 -0600

    added current day and save on click to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 
 
 
-var currentDateAndTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+var currentDateAndTime = moment().format('MMMM Do YYYY, h:mm: a');
 
-console.log(currentDateAndTime);
+$('#currentDay').text(currentDateAndTime);
+
+
+// save on click
+
+$('.saveBtn').on('click', function() {
+    
+        var text = $(this).siblings('.description').val();
+    
+        var time = $(this).parent().attr('id');
+    
+        localStorage.setItem(time, text);
+    
+    });
+    
